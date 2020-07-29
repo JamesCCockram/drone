2b87e03cd46bf8fb94f433987f28fa33bf6ecc8d
James Cockram <jamesccockram16@gmail.com>
2020-07-29 12:08:23 +1000

    Updated Node

--- node/server.js
+++ node/server.js
@@ -1,3 +1,16 @@
 var arDrone = require('ar-drone');
-var client  = arDrone.createClient();
-client.createRepl();
+var PaVEParser = require('ar-drone/lib/video/PaVEParser');
+var output = require('fs').createWriteStream('./vid.h264');
+
+var video = arDrone.createClient().getVideoStream();
+var parser = new PaVEParser();
+
+parser
+  .on('data', function(data) {
+    output.write(data.payload);
+  })
+  .on('end', function() {
+    output.end();
+  });
+
+video.pipe(parser);
