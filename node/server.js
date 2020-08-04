6eaf12fe5c2031f73798423cf473dcd6b97c8703
James Cockram <jamesccockram16@gmail.com>
2020-08-04 21:45:12 +1000

    Added more commands

--- node/server.js
+++ node/server.js
@@ -17,6 +17,47 @@ app.get('/land', function(req, res){
   res.send('Drone has Landed!')
 })
 
+//Make drone go left
+app.get('/left', function(req, res){
+  client.left(1)
+  res.send('Drone is moving to the left')
+})
+
+//Make drone go right
+app.get('/right', function(req, res){
+  client.right(1)
+  res.send('Drone is moving to the right')
+})
+
+//Make drone go forward
+app.get('/forward', function(req, res){
+  client.front(1)
+  res.send('Drone is moving forward')
+})
+
+//Make drone go back
+app.get('/back', function(req, res){
+  client.back(1)
+  res.send('Drone is moving back')
+})
+
+//Make drone go up
+app.get('/up', function(req, res){
+  client.up(1)
+  res.send('Drone is going up')
+})
+
+//Make drone go down
+app.get('/down', function(req, res){
+  client.down(1)
+  res.send('Drone is going down')
+})
+
+//Make drone stop
+app.get('/stop', function(req, res){
+  client.stop()
+  res.send('Drone has Stopped!')
+})
 
 //Start ExpressJS server
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- python/drone.py
+++ python/drone.py
@@ -6,5 +6,26 @@ def takeoff():
 def land():
     requests.get('http://localhost:3000/land')
 
+def left():
+    requests.get('http://localhost:3000/left')
+
+def right():
+    requests.get('http://localhost:3000/right')
+
+def forward():
+    requests.get('http://localhost:3000/forward')
+
+def back():
+    requests.get('http://localhost:3000/back')
+
+def up():
+    requests.get('http://localhost:3000/up')
+
+def down():
+    requests.get('http://localhost:3000/down')
+
+def stop():
+    requests.get('http://localhost:3000/stop') 
+
 takeoff()
 land()
